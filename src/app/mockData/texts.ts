87b2cfce6a305de3394f7604f690ecba49aebece
dragosgry89 <dragos.grigore89@gmail.com>
2026-02-09 22:30:05 +0200

    Created mock texts for error messages

---
+++ src/app/mockData/texts.ts
@@ -0,0 +1,3 @@
+export const MOCK_TEXT = {
+    GET_BOOKMARKS_ERROR: 'An error occurred - Make sure JSON server is running...'
+}
--- src/app/store/effects/effects.ts
+++ src/app/store/effects/effects.ts
@@ -4,6 +4,7 @@ import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { GetBookmarks, GetBookmarksError, GetBookmarksSuccess } from "../actions/actions";
 import { BookmarksService } from "../../services/bookmarks.service";
 import { catchError, map, of, switchMap } from "rxjs";
+import { MOCK_TEXT } from "../../mockData/texts";
 
 @Injectable()
 export class BookmarksEffects {
@@ -18,7 +19,7 @@ export class BookmarksEffects {
             switchMap(() => 
                 this.bookmarksService.getBookmarks().pipe(
                     map((bookmarks) => GetBookmarksSuccess({ bookmarks })),
-                    catchError(() => of(GetBookmarksError({ error: 'An error occurred - Make sure JSON server is running...' })))
+                    catchError(() => of(GetBookmarksError({ error: MOCK_TEXT.GET_BOOKMARKS_ERROR })))
                 )
             )
         )
